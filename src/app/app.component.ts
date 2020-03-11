b4e4f163edf754f1ca4c51daac35ad196742d420
fbleite <feliperbleite@gmail.com>
2020-03-10 21:56:13 -0400

    Added a post for new recipes all ugly but works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,26 @@ import { HttpClient } from '@angular/common/http'
 })
 export class AppComponent {
   title = 'client';
-    recipes = {};
+    recipes = [];
     constructor(private http: HttpClient) {
-      http.get('recipes').subscribe(recipes => this.recipes = recipes);
+      http.get('recipes').subscribe(recipesWeb => {
+        let recipesArray = recipesWeb as {}[]
+        recipesArray.forEach(recipe => this.recipes.push(recipe));
+        // this.recipes.push(recipes))
+      })
     }
+
+    addRecipe(recipeName : string, recipeCategory: string) {
+      var recipe = {"name": recipeName, "category": recipeCategory}
+      this.recipes.push(recipe)
+
+      let headers = new Headers();
+      headers.append('Content-Type', 'application/json');
+      // this.http.post('recipes', recipe, {headers : headers})
+      this.http.post('recipes', recipe)
+        .subscribe(res => {
+           console.log('inside postmehtod of sub.function');//only objects
+        })
+    }
+
 }
